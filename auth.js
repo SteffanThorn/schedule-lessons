1b224c72d53febf14712f5b049c3054af68084d4
stee <stee>
2026-01-25 14:11:00 +1300

    Remove MongoDB adapter for now - stored in auth.with-mongodb.js for later re-enable

--- auth.js
+++ auth.js
@@ -1,8 +1,5 @@
 import NextAuth from "next-auth"
-import { MongoDBAdapter } from "@auth/mongodb-adapter"
-import clientPromise from "@/libs/mongo"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: MongoDBAdapter(clientPromise),
   providers: [],
 })
